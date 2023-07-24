c90940350d98a58574cb2fe6da28db328065545d
Julio189 <julio.c.jesus@hotmail.com>
2023-07-24 15:45:51 -0300

    feat: add yup validated in create controller

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,11 +1,28 @@
 import { Request, Response } from 'express'
+import * as yup from 'yup'
 
 interface ICidade {
   nome: string
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-  console.log(req.body.nome)
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+})
 
-  return res.send('Create')
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validatedData: ICidade | undefined
+  try {
+    validatedData = await bodyValidation.validate(req.body)
+  } catch (error) {
+    const yupError = error as yup.ValidationError
+    return res.json({
+      errors: {
+        default: yupError.message,
+      },
+    })
+  }
+
+  console.log(validatedData)
+
+  return res.send('Create!')
 }
