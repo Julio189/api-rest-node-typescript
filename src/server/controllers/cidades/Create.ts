f016d58c3bd345dfc5b0b3af4748aaa7fca2d05a
Julio189 <julio.c.jesus@hotmail.com>
2023-07-25 18:25:43 -0300

    feat: add all validation for field

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,24 +1,36 @@
 import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
 import * as yup from 'yup'
 
 interface ICidade {
   nome: string
+  estado: string
 }
 
 const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 })
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined
   try {
-    validatedData = await bodyValidation.validate(req.body)
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    })
   } catch (error) {
     const yupError = error as yup.ValidationError
-    return res.json({
-      errors: {
-        default: yupError.message,
-      },
+    const errors: Record<string, string> = {}
+
+    yupError.inner.forEach((error) => {
+      // if(error.path === undefined)
+      if (!error.path) return
+
+      errors[error.path] = error.message
+    })
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors,
     })
   }
 
