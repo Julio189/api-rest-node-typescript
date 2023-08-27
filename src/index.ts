5d4aee3ef68d638403f2d30d5b3b1ef0cb01249f
Julio189 <julio.c.jesus@hotmail.com>
2023-08-27 11:24:56 -0300

    edit: rename console log

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from './server/Server'
 
 server.listen(process.env.PORT_URL || 3333, () =>
-  console.log(`app rodando na porta ${process.env.PORT_URL || 3333}`),
+  console.log(`server rodando na porta ${process.env.PORT_URL || 3333}`),
 )
